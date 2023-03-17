cdbf7cd2669097e90f82d587e504203354c44060
Tomislav Eric <hi@tomislav.mobi>
2023-03-17 22:34:19 +0100

    add initializer

--- Sources/SharedModels/Skill.swift
+++ Sources/SharedModels/Skill.swift
@@ -1,6 +1,10 @@
 import Foundation
 
 public struct Skill: Equatable, Identifiable, Codable {
+    public init(points: Double, zoneType: ZoneType) {
+        self.points = points
+        self.zoneType = zoneType
+    }
     public var id: UUID { UUID() }
     public let points: Double
     public let zoneType: ZoneType
