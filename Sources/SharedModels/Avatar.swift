638cc595c3bb5799d42003e9b87c02a07e8ad613
Tomislav Eric <hi@tomislav.mobi>
2023-03-18 23:00:25 +0100

    make id optional

--- Sources/SharedModels/Avatar.swift
+++ Sources/SharedModels/Avatar.swift
@@ -1,17 +1,15 @@
 import Foundation
 
 public struct Avatar: Equatable, Codable, Identifiable {
-    public init(id: UUID, name: String? = nil, age: Int? = nil, skills: [Skill]? = nil, isDefault: Bool = false) {
+    public init(id: UUID? = nil, name: String? = nil, age: Int? = nil, skills: [Skill]? = nil) {
         self.id = id
         self.name = name
         self.age = age
         self.skills = skills
-        self.isDefault = isDefault
     }
     
-    public let id: UUID
+    public let id: UUID?
     public let name: String?
     public let age: Int?
     public let skills: [Skill]?
-    public let isDefault: Bool
 }
