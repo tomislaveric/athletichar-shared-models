10e84720f87d9f001d1a24eacb74b3e8a8a7f526
Tomislav Eric <hi@tomislav.mobi>
2023-03-18 19:16:59 +0100

    make Avatar identifiable

--- Sources/SharedModels/Avatar.swift
+++ Sources/SharedModels/Avatar.swift
@@ -1,15 +1,17 @@
 import Foundation
 
-public struct Avatar: Equatable, Codable {
-    public init(id: UUID, name: String? = nil, age: Int? = nil, skills: [Skill]? = nil) {
+public struct Avatar: Equatable, Codable, Identifiable {
+    public init(id: UUID, name: String? = nil, age: Int? = nil, skills: [Skill]? = nil, isDefault: Bool = false) {
         self.id = id
         self.name = name
         self.age = age
         self.skills = skills
+        self.isDefault = isDefault
     }
     
     public let id: UUID
     public let name: String?
     public let age: Int?
     public let skills: [Skill]?
+    public let isDefault: Bool
 }
